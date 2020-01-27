baafed8622e04d9c8da9c43a7d937ee047c8799c
lgalle <lucie.galle@capgemini.com>
2020-01-27 17:55:21 +0100

    commit

--- contracts/solidity/Crowdfunding.sol
+++ contracts/solidity/Crowdfunding.sol
@@ -5,6 +5,12 @@ struct investor {
     int investment;
 }
 
+struct Tier {
+    int inf_bound;
+    int sup_bound;
+    ufixed interest_rate; 
+}
+
 contract mortal {
     address owner;
     constructor() public { owner = msg.sender; }
@@ -13,4 +19,11 @@ contract mortal {
 contract crownfunding is mortal {
     uint counter;
     investor[] investors;
+    Tier[] tiers;
+    Tier t = Tier(1, 10, 1.05);
+    ufixed[] interest_rates = [1.05,1.10,1.20];
+
+    int constant investment_tier = 1000;
+
+
 }
