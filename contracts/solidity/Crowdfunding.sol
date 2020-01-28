3d8d2e6b7295da7624e5b74c60f142d5a975764f
Cioccoffee <gallelucie@gmail.com>
2020-01-28 09:19:59 +0100

    minor advancement

--- contracts/solidity/Crowdfunding.sol
+++ contracts/solidity/Crowdfunding.sol
@@ -19,12 +19,12 @@ contract mortal {
 contract crowdfunding is mortal {
     uint counter;
     investor[] investors;
-    Tier[] tiers;
-    Tier t = Tier(1, 10, 105);
+    Tier[] tiers = [Tier(1, 10, 105), Tier(11, 25, 110), Tier(1, 10, 120)];
+    int constant investment_goal = 1000;
+    int current_investment = 0;
 
-    int constant investment_tier = 1000;
+    function invest(int money) public pure returns (bool){
 
-    function invest() public pure returns (int) {
-        return 1;
+        return true;
     }
 }
